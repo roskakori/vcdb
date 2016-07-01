1a43f01157ea67c231e640fae10bb1e8a3b5b06b
Thomas Aglassinger <roskakori@users.sourceforge.net>
2016-07-01 07:21:06 +0200

    Added check for minimum Python version.

--- vcdb/__init__.py
+++ vcdb/__init__.py
@@ -3,4 +3,11 @@ Vcdb module initialization.
 """
 # Copyright (C) 2016 Thomas Aglassinger.
 # Distributed under the GNU Lesser General Public License v3 or later.
+
+# Ensure minimum Python version.
+import sys
+if sys.version_info[:2] < (3, 4):
+    sys.exit('Module vcdb requires Python version 3.4 or later')
+
+
 __version__ = '0.0.1'
